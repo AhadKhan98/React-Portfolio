15fd88b3ef6580ef8c99aa0edd046d3e15f4eb2c
AhadKhan98 <ahad_zai@berea.edu>
2020-11-10 23:45:32 +0500

    move files into folder^ clean up app component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my portfolio!</h1>
+    </div>
+  );
+}
+
+export default App;
