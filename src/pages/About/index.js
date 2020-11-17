aa6f6a64aabb51693d37bd65b45ca8277a31ecc5
AhadKhan98 <ahad_zai@berea.edu>
2020-11-17 15:38:39 +0500

    fix up  about me

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -12,13 +12,13 @@ const About = () => {
             <div className="row">
                 <div className="column">
                     <p>
-                        I am a highly motivated, and a self-taught Software Engineer.
+                        I am a highly motivated, and self-taught Software Engineer.
                         <br /> <br />
-                        When I was a child, I would break apart various devices and toys simply to disinter their internal workings and scrutinize their core mechanisms. As I grew older, the inquisitiveness became more potent and thus led me to explore different aspects of Computer Science and develop a meticulous understanding of its prominent facets, such as, Software Engineering, Data Science, Web Development, and Mobile-App Development.
+                        My passion for engineering goes all the way back to my childhood, as I would break apart various devices and toys simply to disinter their internal workings and scrutinize their core mechanisms. As I grew older, the inquisitiveness became more potent and thus led me to explore different aspects of Computer Science and develop a meticulous understanding of its prominent facets, such as, Software Engineering, Data Science, Web Development, and Mobile-App Development.
                         <br /><br />
-                        I am an easy-going person, and very adaptable at the same time. Working alongside new teams is something that I not only am comfortable with, but greatly enjoy.
-                        <br /><br />
-                        With a strong belief in lifelong learning for both personal and professional reasons, nothing brings me more joy than learning about something that is completely new to me. Whether that be a new programming language, or a new chess opening.
+                        I am an easy-going and adaptable person; I thoroughly enjoy working alongside new teams as it provides me with more opportunities to learn.
+                        <br />
+                        With a strong belief in lifelong learning for both personal and professional reasons, nothing brings me more joy than discovering new concepts. Whether that be a new programming language, or a new chess opening.
                     </p>
                 </div>
             </div>
